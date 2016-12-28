3aecbfd617b0a33ddbeac7816cde33177e5e7789
Rico Ullmann <rullmann@seibert-media.net>
2016-12-28 19:31:07 +0100

    add collectd integration

--- items.py
+++ items.py
@@ -10,3 +10,20 @@ svc_systemd = {
         ],
     },
 }
+
+files = {}
+
+if node.has_bundle("collectd"):
+    files['/etc/collectd.d/hddtemp.conf'] = {
+        'source': "collectd.conf",
+        'mode': "0640",
+        'owner': "root",
+        'group': "root",
+        'needs': [
+            "pkg_dnf:hddtemp",
+            "svc_systemd:hddtemp",
+        ],
+        'triggers': [
+            "svc_systemd:collectd:restart",
+        ],
+    }
