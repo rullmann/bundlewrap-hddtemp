4a860c992ce31878eed12816e8d7b1f8f0e96403
Rico Ullmann <rico@erinnerungsfragmente.de>
2017-09-30 19:01:31 +0200

    update to bw 3.0

--- items.py
+++ items.py
@@ -4,7 +4,6 @@ pkg_dnf = {
 
 svc_systemd = {
     'hddtemp': {
-        'enabled': True,
         'needs': [
             "pkg_dnf:hddtemp",
         ],
@@ -17,8 +16,6 @@ if node.has_bundle("collectd"):
     files['/etc/collectd.d/hddtemp.conf'] = {
         'source': "collectd.conf",
         'mode': "0640",
-        'owner': "root",
-        'group': "root",
         'needs': [
             "pkg_dnf:hddtemp",
             "svc_systemd:hddtemp",
