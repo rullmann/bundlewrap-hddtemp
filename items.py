c9a2bb7eef88161f757c1ecfabe8be6e23a054a7
Rico Ullmann <rico@erinnerungsfragmente.de>
2017-09-30 23:04:59 +0200

    streamline needs and triggers

--- items.py
+++ items.py
@@ -4,9 +4,7 @@ pkg_dnf = {
 
 svc_systemd = {
     'hddtemp': {
-        'needs': [
-            'pkg_dnf:hddtemp',
-        ],
+        'needs': ['pkg_dnf:hddtemp'],
     },
 }
 
@@ -16,11 +14,6 @@ if node.has_bundle('collectd'):
     files['/etc/collectd.d/hddtemp.conf'] = {
         'source': 'collectd.conf',
         'mode': '0640',
-        'needs': [
-            'pkg_dnf:hddtemp',
-            'svc_systemd:hddtemp',
-        ],
-        'triggers': [
-            'svc_systemd:collectd:restart',
-        ],
+        'needs': [ 'pkg_dnf:hddtemp', 'svc_systemd:hddtemp'],
+        'triggers': ['svc_systemd:collectd:restart'],
     }
