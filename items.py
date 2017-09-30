525f24916bfc4c04f1f62efd0493bc3dbf0a92d2
Rico Ullmann <rico@erinnerungsfragmente.de>
2017-09-30 20:06:13 +0200

    proper formatting everywhere

--- items.py
+++ items.py
@@ -5,22 +5,22 @@ pkg_dnf = {
 svc_systemd = {
     'hddtemp': {
         'needs': [
-            "pkg_dnf:hddtemp",
+            'pkg_dnf:hddtemp',
         ],
     },
 }
 
 files = {}
 
-if node.has_bundle("collectd"):
+if node.has_bundle('collectd'):
     files['/etc/collectd.d/hddtemp.conf'] = {
-        'source': "collectd.conf",
-        'mode': "0640",
+        'source': 'collectd.conf',
+        'mode': '0640',
         'needs': [
-            "pkg_dnf:hddtemp",
-            "svc_systemd:hddtemp",
+            'pkg_dnf:hddtemp',
+            'svc_systemd:hddtemp',
         ],
         'triggers': [
-            "svc_systemd:collectd:restart",
+            'svc_systemd:collectd:restart',
         ],
     }
